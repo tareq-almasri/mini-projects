3155a1095b5330a29e62642c2704e8ed625e6f5b
Tareq Almasri <47785561+tareq-almasri@users.noreply.github.com>
2020-05-12 15:44:01 +0200

    Update flappyBird.js

--- flappybird/flappyBird.js
+++ flappybird/flappyBird.js
@@ -78,7 +78,7 @@ function draw(){
 
         // detect collision
         
-        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
+        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height ){
             location.reload(); // reload the page
         }
         
